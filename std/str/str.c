347f9cedb38e7e4ba2cf7e63f720930804570202
lnrif <lnrifx@gmail.com>
2026-08-30 17:30:42 +0300

    str: small refactor

--- std/str/str.c
+++ std/str/str.c
@@ -8,20 +8,6 @@ u8 * str_find_byte(Str s, u8 b) {
 	return memchr(s.ptr, s.len, b);
 };
 
-u64 len_z(u8 const * s, u64 limit) {
-	u8 const * at = memchr(s, limit, 0);
-	if (at == 0) return limit;
-	return (u64)(at - s);
-};
-
-StrZ str_z_init(u8z const * ptr_z) {
-	return STR_Z(ptr_z, (u64)ptr_z - (u64)memchr(ptr_z, U64_MAX, 0));
-};
-
-b8 str_is_none_ex(Str const * str) {
-	return str->ptr == 0;
-};
-
 // |================================================================================================|
 // |> [Str]: comparision                                                                            |
 
@@ -30,3 +16,15 @@ b8 str_eq(Str a, Str b) {
 	return memcmp(a.ptr, b.ptr, a.len) == 0;
 };
 
+// |================================================================================================|
+// |> [StrZ]: init                                                                                  |
+
+StrZ str_z_init(u8z const * ptr_z) {
+	return str_z_limit(ptr_z, U64_MAX);
+};
+
+StrZ str_z_limit(u8z const * ptr_z, u64 limit) {
+	u8 const * at = memchr(ptr_z, limit, 0);
+	if (at == 0) at = ptr_z + limit;
+	return STR_Z(ptr_z, (u64)(ptr_z - at));
+};
--- std/str/str.h
+++ std/str/str.h
@@ -20,6 +20,33 @@ typedef union {
 	};
 } StrMut;
 
+#define STR(_ptr, _len) ((Str){.ptr = (_ptr), .len = (_len)})
+#define STR_NIL STR(0, 0)
+#define S(lit)  STR((u8*)(void*)(lit), sizeof(lit) - 1)
+
+#define STR_MUT(_ptr, _len) ((StrMut){.ptr = (_ptr), .len = (_len)})
+#define STR_MUT_NIL STR_MUT(0, 0)
+
+// |================================================================================================|
+// |> [Str]: find                                                                                   |
+
+u8 * str_find_byte(Str s, u8 b);
+
+// |================================================================================================|
+// |> [Str]: comparision                                                                            |
+
+b8 str_eq(Str a, Str b);
+#define str_eq_lit(x, lit) str_eq(x, S(lit))
+
+// |================================================================================================|
+// |> [Str]: properties                                                                             |
+
+#define str_is_nil(x) ((x)->ptr == 0)
+
+// |================================================================================================|
+// |> C-STRING                                                                                      |
+// |================================================================================================|
+
 typedef union {
 	Str str;
 	struct {
@@ -32,29 +59,15 @@ typedef union {
 	};
 } StrZ;
 
-#define STR_NONE     ((Str   ){.ptr = 0, .len = 0})
-#define STR_Z_NONE   ((StrZ  ){.ptr = 0, .len = 0})
-#define STR_MUT_NONE ((StrMut){.ptr = 0, .len = 0})
+// |================================================================================================|
+// |> [StrZ]: init                                                                                  |
+
+#define STR_Z(_ptr_z, _len) ((StrZ){.ptr = (_ptr_z), .len = (_len)})
+
+#define STR_Z_NIL STR_Z(0, 0)
+#define Z(lit) STR_Z((u8*)(void*)(lit), sizeof(lit) - 1)
 
 StrZ str_z_init(u8z const * ptr_z);
-
-#define str_is_none(x) ((x)->ptr == 0)
-
-#define STR(_ptr, _len)     ((Str ){.ptr   = (_ptr  ), .len = (_len)})
-#define STR_Z(_ptr_z, _len) ((StrZ){.ptr_z = (_ptr_z), .len = (_len)})
-
-#define S(lit) ((Str ){.ptr = (u8*)(void*)(lit), .len = sizeof(lit) - 1})
-#define Z(lit) ((StrZ){.ptr = (u8*)(void*)(lit), .len = sizeof(lit) - 1})
-
-// |================================================================================================|
-// |> [Str]: find                                                                                   |
-
-u8 * str_find_byte(Str s, u8 b);
-
-// |================================================================================================|
-// |> [Str]: comparision                                                                            |
-
-b8 str_eq(Str a, Str b);
-#define str_eq_lit(x, lit) str_eq(x, S(lit))
+StrZ str_z_limit(u8z const * ptr_z, u64 limit);
 
 #endif // !STD_STR_H
